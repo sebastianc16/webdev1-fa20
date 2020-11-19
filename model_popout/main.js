d2b17d538252c33e01e13097aa9f58167ad981eb
sebastianc16 <71098006+sebastianc16@users.noreply.github.com>
2020-11-18 21:39:43 -0600

    fix

--- model_popout/main.js
+++ model_popout/main.js
@@ -1,11 +1,24 @@
-$(function() {
-    $(".button").click(function() {
-        $("#myform #valueFromMyButton").text($(this).val().trim());
-        $("#myform input[type=text]").val('');
-        $("#myform").show(500);
-    });
-    $("#btnOK").click(function() {
-        $("#valueFromMyModal").val($("#myform input[type=text]").val().trim());
-        $("#myform").hide(400);
-    });
-});
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
